607a0dddb0646c9274ba6921573c72c73bad9a95
RodrigoAiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-02-17 18:58:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pytz
 # --- CONFIGURAÇÕES DE PÁGINA ---
 st.set_page_config(page_title="SkyData | Analytics Portal", layout="wide", initial_sidebar_state="expanded")
 
-num = 4242
+num = 1
 st.markdown("""
     <style>
     .main {
